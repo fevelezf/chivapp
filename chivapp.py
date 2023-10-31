d6416b5e78e2745748009b8dbc157e88c7f545d6
Felipe Vélez Fernández <fevelezf@unal.edu.co>
2023-10-31 15:16:42 -0500

    Update chivapp.py

--- chivapp.py
+++ chivapp.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 # Lista de opciones para los menús desplegables de origen y destino
-ciudades = ["Ciudad A", "Ciudad B", "Ciudad C", "Ciudad D"]  # Puedes agregar más ciudades
+ciudades = ["Medellin", "San Pedro", "Concepcion", "Abejorral", "La Ceja", "Venecia", "Rionegro"]  # Puedes agregar más ciudades
 
 st.title("¡Bienvenido a tu Agencia de Viajes!")
 st.write("Selecciona tu origen y destino para encontrar tu próximo viaje.")
@@ -9,7 +9,7 @@ st.write("Selecciona tu origen y destino para encontrar tu próximo viaje.")
 origen = st.selectbox("Origen:", ciudades)
 destino = st.selectbox("Destino:", ciudades)
 
-if st.button("Buscar vuelos"):
+if st.button("Busca Tu Viaje"):
     if origen == destino:
         st.warning("El origen y el destino no pueden ser iguales. Por favor, selecciona ciudades diferentes.")
     else:
