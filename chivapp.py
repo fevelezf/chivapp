8dfd296c281d9483a67e505ee4597da1241230c7
Felipe Vélez Fernández <fevelezf@unal.edu.co>
2023-10-31 15:33:43 -0500

    Update chivapp.py

--- chivapp.py
+++ chivapp.py
@@ -10,14 +10,7 @@ def pagina_inicio():
     destino = st.selectbox("Destino:", ciudades)
     personas = st.number_input("¿Cuántas personas viajan?", min_value=1, max_value=15, step=1)
 
-    if st.button("Busca Tu Viaje"):
-        if origen == destino:
-            st.warning("El origen y el destino no pueden ser iguales. Por favor, selecciona ciudades diferentes.")
-            
-        else:
-            st.success(f"Buscando vuelos desde {origen} a {destino}. ¡Pronto tendrás opciones disponibles!")
-            return origen, destino, personas
-            
+    return origen, destino, personas
 
 def pagina_reserva(origen, destino, personas):
     st.header("Reserva para personas:")
@@ -28,14 +21,19 @@ def pagina_reserva(origen, destino, personas):
         correo = st.text_input(f"Correo de la persona {i + 1}")
 
 def main():
-    opcion = st.selectbox("Navegación", ["Inicio", "Reserva"])
+    opcion = st.sidebar.selectbox("Navegación", ["Inicio", "Reserva"])
 
     if opcion == "Inicio":
         origen, destino, personas = pagina_inicio()
-        pagina_reserva(origen, destino, personas)
-        
-    elif opcion == "Reserva":
-        pagina_reserva(origen, destino, personas)
+        st.session_state.origen = origen
+        st.session_state.destino = destino
+        st.session_state.personas = personas
+
+    if opcion == "Reserva":
+        if 'origen' in st.session_state and 'destino' in st.session_state and 'personas' in st.session_state:
+            pagina_reserva(st.session_state.origen, st.session_state.destino, st.session_state.personas)
+        else:
+            st.warning("Primero selecciona el origen, destino y número de personas en la página de Inicio.")
 
 if __name__ == "__main__":
     main()
