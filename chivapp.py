fbd8985c873d0eee7c730903a7b811f6f26af725
Felipe Vélez Fernández <fevelezf@unal.edu.co>
2023-10-31 15:31:48 -0500

    Update chivapp.py

--- chivapp.py
+++ chivapp.py
@@ -1,41 +1,32 @@
 import streamlit as st
 
-def pagina_inicio():
-    ciudades = ["Medellin", "San Pedro", "Concepcion", "Abejorral", "La Ceja", "Venecia", "Rionegro"]
+class AplicacionViajes:
+    def pagina_inicio(self):
+        ciudades = ["Medellin", "San Pedro", "Concepcion", "Abejorral", "La Ceja", "Venecia", "Rionegro"]
 
-    st.title("¡Bienvenido a tu Agencia de Viajes!")
-    st.write("Selecciona tu origen y destino para encontrar tu próximo viaje.")
+        st.title("¡Bienvenido a tu Agencia de Viajes!")
+        st.write("Selecciona tu origen y destino para encontrar tu próximo viaje.")
 
-    origen = st.selectbox("Origen:", ciudades)
-    destino = st.selectbox("Destino:", ciudades)
-    personas = st.number_input("¿Cuántas personas viajan?", min_value=1, max_value=15, step=1)
+        self.origen = st.selectbox("Origen:", ciudades)
+        self.destino = st.selectbox("Destino:", ciudades)
+        self.personas = st.number_input("¿Cuántas personas viajan?", min_value=1, max_value=15, step=1)
 
-    if st.button("Busca Tu Viaje"):
-        if origen == destino:
-            st.warning("El origen y el destino no pueden ser iguales. Por favor, selecciona ciudades diferentes.")
-            
-        else:
-            st.success(f"Buscando vuelos desde {origen} a {destino}. ¡Pronto tendrás opciones disponibles!")
-            return origen, destino, personas
-            
-
-def pagina_reserva(origen, destino, personas):
-    st.header("Reserva para personas:")
-    for i in range(personas):
-        st.subheader(f"Datos de la persona {i + 1}")
-        nombre = st.text_input(f"Nombre de la persona {i + 1}")
-        cedula = st.text_input(f"Cédula de la persona {i + 1}")
-        correo = st.text_input(f"Correo de la persona {i + 1}")
+    def pagina_reserva(self):
+        st.header("Reserva para personas:")
+        for i in range(self.personas):
+            st.subheader(f"Datos de la persona {i + 1}")
+            nombre = st.text_input(f"Nombre de la persona {i + 1}")
+            cedula = st.text_input(f"Cédula de la persona {i + 1}")
+            correo = st.text_input(f"Correo de la persona {i + 1}")
 
 def main():
-    opcion = st.selectbox("Navegación", ["Inicio", "Reserva"])
+    app = AplicacionViajes()
+    opcion = st.sidebar.radio("Navegación", ["Inicio", "Reserva"])
 
     if opcion == "Inicio":
-        origen, destino, personas = pagina_inicio()
-        pagina_reserva(origen, destino, personas)
-        
+        app.pagina_inicio()
     elif opcion == "Reserva":
-        pagina_reserva(origen, destino, personas)
+        app.pagina_reserva()
 
 if __name__ == "__main__":
     main()
