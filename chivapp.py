742d50be59a686411350f2ccf71b38dae9546ab6
Felipe Vélez Fernández <fevelezf@unal.edu.co>
2023-10-31 15:39:02 -0500

    Update chivapp.py

--- chivapp.py
+++ chivapp.py
@@ -29,10 +29,7 @@ def main():
         st.session_state.destino = destino
         st.session_state.personas = personas
         if st.button('Reverva Right Now'):
-            if 'origen' in st.session_state and 'destino' in st.session_state and 'personas' in st.session_state:
-                pagina_reserva(st.session_state.origen, st.session_state.destino, st.session_state.personas)
-            else:
-                st.warning("Primero selecciona el origen, destino y número de personas en la página de Inicio.")
+            st.session_state.pagina_actual = "Reserva"
 
     if opcion == "Reserva":
         if 'origen' in st.session_state and 'destino' in st.session_state and 'personas' in st.session_state:
