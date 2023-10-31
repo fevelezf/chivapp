0c89916057043c7fda9f443a355f62a0b141e21f
Felipe Vélez Fernández <fevelezf@unal.edu.co>
2023-10-31 15:25:41 -0500

    Update chivapp.py

--- chivapp.py
+++ chivapp.py
@@ -1,16 +1,39 @@
 import streamlit as st
 
-# Lista de opciones para los menús desplegables de origen y destino
-ciudades = ["Medellin", "San Pedro", "Concepcion", "Abejorral", "La Ceja", "Venecia", "Rionegro"]  # Puedes agregar más ciudades
+def pagina_inicio():
+    ciudades = ["Medellin", "San Pedro", "Concepcion", "Abejorral", "La Ceja", "Venecia", "Rionegro"]
 
-st.title("¡Bienvenido a tu Agencia de Viajes!")
-st.write("Selecciona tu origen y destino para encontrar tu próximo viaje.")
+    st.title("¡Bienvenido a tu Agencia de Viajes!")
+    st.write("Selecciona tu origen y destino para encontrar tu próximo viaje.")
 
-origen = st.selectbox("Origen:", ciudades)
-destino = st.selectbox("Destino:", ciudades)
+    origen = st.selectbox("Origen:", ciudades)
+    destino = st.selectbox("Destino:", ciudades)
+    personas = st.number_input("¿Cuántas personas viajan?", min_value=1, max_value=15, step=1)
 
-if st.button("Busca Tu Viaje"):
-    if origen == destino:
-        st.warning("El origen y el destino no pueden ser iguales. Por favor, selecciona ciudades diferentes.")
-    else:
-        st.success(f"Buscando vuelos desde {origen} a {destino}. ¡Pronto tendrás opciones disponibles!")
+    if st.button("Busca Tu Viaje"):
+        if origen == destino:
+            st.warning("El origen y el destino no pueden ser iguales. Por favor, selecciona ciudades diferentes.")
+        else:
+            st.success(f"Buscando vuelos desde {origen} a {destino}. ¡Pronto tendrás opciones disponibles!")
+            pagina_reserva(origen, destino, personas)
+            
+
+def pagina_reserva(origen, destino, personas):
+    st.header("Reserva para personas:")
+    for i in range(personas):
+        st.subheader(f"Datos de la persona {i + 1}")
+        nombre = st.text_input(f"Nombre de la persona {i + 1}")
+        cedula = st.text_input(f"Cédula de la persona {i + 1}")
+        correo = st.text_input(f"Correo de la persona {i + 1}")
+
+def main():
+    opcion = st.sidebar.radio("Navegación", ["Inicio", "Reserva"])
+
+    if opcion == "Inicio":
+        origen, destino, personas = pagina_inicio()
+        
+    elif opcion == "Reserva":
+        pagina_reserva(origen, destino, personas)
+
+if __name__ == "__main__":
+    main()
