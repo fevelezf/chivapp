3f4a03c595dbee9b9372ce3622e6cbebb58cc0dd
Felipe Vélez Fernández <fevelezf@unal.edu.co>
2023-11-20 21:16:57 -0500

    Update chivapp.py

--- chivapp.py
+++ chivapp.py
@@ -176,11 +176,13 @@ elif selected_option == 'Busqueda de viajes':
     if origen is not None:
         # Realizar acciones adicionales o llamar a otras funciones según sea necesario
         pagina_reserva(personas)
-        #if pagina_reserva:
-        #    pago()
+
     else:
         # Manejar el caso en el que no se selecciona un viaje
         st.warning("Por favor, selecciona un viaje antes de continuar.")
+
+    if pagina_reserva:
+        pago()
 
 
 elif selected_option == 'Busqueda de chiva Rumbera':
