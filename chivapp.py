97e6a54161495fe4165c8e3edddee7cd2ad5d9dd
Felipe Vélez Fernández <fevelezf@unal.edu.co>
2023-11-22 10:39:04 -0500

    Update chivapp.py

--- chivapp.py
+++ chivapp.py
@@ -458,7 +458,7 @@ if get_current_user() is not None:
 
             pagina_reserva(num, personas, origen, destino, correo)
 
-        if menu_option == 'Pagar reserva':
+        if menu_option == "Pagar Reserva":
             correo = r['correo']
             origen = r['origen']
             destino = r['destino']
